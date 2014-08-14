07e1d05cd287e43084a9da2df1a753cb8bd76d4c
Gabe Fierro <gtfierro225@gmail.com>
2014-08-14 12:07:32 -0700

    indents

--- ReadingDB.rb
+++ ReadingDB.rb
@@ -1,26 +1,26 @@
 require 'formula'
 
 class ReadingDB < Formula
-    homepage 'https://github.com/SoftwareDefinedBuildings/readingdb'
+  homepage 'https://github.com/SoftwareDefinedBuildings/readingdb'
 
-    head 'https://github.com/SoftwareDefinedBuildings/readingdb.git', :branch => 'adaptive'
+  head 'https://github.com/SoftwareDefinedBuildings/readingdb.git', :branch => 'adaptive'
 
-    depends_on 'lbdb'
-    depends_on 'protobuf'
-    depends_on 'protobuf-c'
-    depends_on 'lzlib'
-    depends_on 'autoconf'
-    depends_on 'automake'
-    depends_on 'swig'
+  #depends_on 'lbdb'
+  #depends_on 'protobuf'
+  #depends_on 'protobuf-c'
+  #depends_on 'lzlib'
+  #depends_on 'autoconf'
+  #depends_on 'automake'
+  #depends_on 'swig'
 
-    def install
-        inreplace 'src/reading-server.c', /inline valid_bucketsize/, 'valid_bucketsize'
-        ENV['CC'] = 'llvm-gcc'
-        ENV['CXX'] = 'llvm-g++'
-        system 'autoreconf', '--install'
-        system './configure'
-        system 'make'
-        system 'make', 'install'
-    end
+  def install
+    inreplace 'src/reading-server.c', /inline valid_bucketsize/, 'valid_bucketsize'
+    ENV['CC'] = 'llvm-gcc'
+    ENV['CXX'] = 'llvm-g++'
+    system 'autoreconf', '--install'
+    system './configure'
+    system 'make'
+    system 'make', 'install'
+  end
 end
 
