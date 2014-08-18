a71fa1cea75478ce4cfeedc7883eddedfce4c71f
Gabe Fierro <gtfierro225@gmail.com>
2014-08-18 16:25:12 -0700

    add dependency on numpy and caveats for pythonpath

--- ReadingDB.rb
+++ ReadingDB.rb
@@ -13,6 +13,7 @@ class Readingdb < Formula
   depends_on 'automake'
   depends_on 'swig'
   depends_on :python
+  depends_on 'numpy' => :python
 
   def install
     inreplace 'src/reading-server.c', /inline int valid_bucketsize/, 'int valid_bucketsize'
@@ -26,6 +27,13 @@ class Readingdb < Formula
     Dir.chdir("python")
     system 'make'
     system "python", 'setup.py', 'install', "--prefix=#{prefix}"
+  end
+  def caveats
+    s = <<-EOS.undent
+    Make sure you add homebrew to your PYTHONPATH. Add the following to your .bash_profile:
+    export PYTHONPATH=`brew --prefix`/lib/python2.7/site-packages:$PYTHONPATH
+    EOS
+    s
   end
 end
 
